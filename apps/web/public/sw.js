fe970f36283858f47fe047fc3e653b960afc0095
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-10 16:16:08 -0400

    chore: service worker v116 for the fixed-bone table

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v115';
+const VERSION = 'yaaddominoes-v116';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
