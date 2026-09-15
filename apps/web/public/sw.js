8d890b3811af7601ad2844434be7409fe2bdf0af
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-15 09:02:01 -0400

    chore: service worker v147 for Stage 2

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v146';
+const VERSION = 'yaaddominoes-v147';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
