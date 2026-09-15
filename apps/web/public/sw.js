87534ce6e82241a23260a557fd3669ec90672d3a
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-15 04:37:36 -0400

    chore: service worker v144 for French pass rule

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v143';
+const VERSION = 'yaaddominoes-v144';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
