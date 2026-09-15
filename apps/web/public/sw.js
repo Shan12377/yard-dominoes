7b7cf61f65d5e4e3c558ee80533de6ab2fa4ef29
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-15 11:29:48 -0400

    chore: service worker v151 for Lounge French and Across

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v150';
+const VERSION = 'yaaddominoes-v151';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
