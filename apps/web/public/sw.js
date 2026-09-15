cbbef4b400b032eb41ab43cda1b700d23b975d07
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-15 04:24:16 -0400

    chore: service worker v143 for JamDom French layout

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v142';
+const VERSION = 'yaaddominoes-v143';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
