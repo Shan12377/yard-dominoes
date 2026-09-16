085eed9753da0f7fdc43ee6f4108434c58b8bd72
Shallanda Hunter <shallandahiggins@yahoo.com>
2026-09-16 08:57:45 -0400

    docs: production is v165

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -12,7 +12,7 @@
  *     network.
  */
 
-const VERSION = 'yaaddominoes-v164';
+const VERSION = 'yaaddominoes-v165';
 const SHELL = `${VERSION}-shell`;
 
 // Bumping VERSION invalidates everything below.
